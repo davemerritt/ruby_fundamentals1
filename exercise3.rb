bed2b5645a73a6881fcb0af93ad9966b614b268a
davemerritt <davidmerritt6@gmail.com>
2013-10-22 13:57:48 -0400

    Your name and age, please.

--- exercise3.rb
+++ exercise3.rb
@@ -1,4 +1,11 @@
 puts "What is your name?"
 name = gets.strip
 
-puts "Hi #{name}" 
+puts "Hi #{name}!"
+puts "How old are you?"
+age = gets.strip
+
+year = 2013
+dob = year - age.to_i
+
+puts "#{name}, you were born in #{dob}!"
