0fe8b9fef61ef6ebbe857e4bffd77caaedc27a5c
davemerritt <davidmerritt6@gmail.com>
2013-10-22 13:31:49 -0400

    Second exercise.

---
+++ exercise2.rb
@@ -0,0 +1,17 @@
+#25% is plenty
+price = 55.0
+percent = 0.25
+tip = (price * percent) + price 
+puts "The total with tip is #{tip}."  
+
+
+x = "4".to_i + 5
+puts x 
+
+y = "45628".to_i
+z = "7839".to_i
+
+puts "Here's a 9 diget number:#{y}" + "#{z}"
+
+q = (true && false) || (false && true) || !(false && false)
+puts q
